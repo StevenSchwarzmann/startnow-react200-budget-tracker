71dbd033b3764441bdc89b7b9d84e5c7962ef164
Steve Schwarzmann <steven.schwarzmann@gmail.com>
2018-04-23 18:13:52 -0700

    Final commit

--- src/js/components/ExpenseEntries/expenseReducer.js
+++ src/js/components/ExpenseEntries/expenseReducer.js
@@ -8,13 +8,9 @@ const defaultState = {
     const { type, payload } = action;
   
     switch (type) {
-      // Here in the case of the update description action 
       case 'UPDATE_EXPENSE_DESCRIPTION': {
-        // we'll return an object
         return {
-          // with all the previous state
           ...state,
-          // but overwriting the description
           description: payload.description
         };
       }
@@ -30,13 +26,8 @@ const defaultState = {
         const { description, amount } = action.payload;
         return {
           description: '',
-          action: '',
-          lineItems: [
-            // here we have all the previous line items
-            ...state.lineItems,
-            // plus a new object
-            { description, amount }
-          ]
+          amount: '',
+          lineItems: [...state.lineItems, { description, amount }]
         };
       }
   
